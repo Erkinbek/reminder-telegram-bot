8d0acc6f463aaa2c743afb2edafe6190cf0ce8ec
Erkin Pardayev <erkin.pardayev@gmail.com>
2019-02-02 17:39:56 +0500

    Bot can receive dates from users

---
+++ common/models/Reminding.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Visual Studio Code.
+ * User: ErkinPardayev
+ * Date: 02.02.2019
+ * Time: 16:31
+ */
+
+namespace common\models;
+
+
+use yii\db\ActiveRecord;
+
+class Reminding extends ActiveRecord
+{
+
+}
